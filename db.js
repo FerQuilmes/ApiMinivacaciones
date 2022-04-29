6b81d8d2b32445c67830cba5f3d3c046dbb3c72c
Fer Quilmes <fernacec@quilmes.com.ar>
2022-04-29 12:40:30 -0300

    config env

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DATABASE_HOST || secrets.DATABASE_HOST,
+  user: process.env.DATABASE_USER || secrets.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD || secrets.DATABASE_PASSWORD,
+  database: process.env.DATABASE || secrets.DATABASE,
   ssl  : {
     // DO NOT DO THIS
     // set up your ca correctly to trust the connection
